5181ab85a80c2ccc000c8c75d86e0b387a82121e
RaviRanjanSus <159718944+RaviRanjanSus@users.noreply.github.com>
2024-02-12 00:21:35 +0530

    Add files via upload

---
+++ springBooot_01/src/main/java/com/ravi/in/SpringBooot01Application.java
@@ -0,0 +1,13 @@
+package com.ravi.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBooot01Application.class, args);
+	}
+
+}
---
+++ springBooot_01/src/test/java/com/ravi/in/SpringBooot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBooot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
